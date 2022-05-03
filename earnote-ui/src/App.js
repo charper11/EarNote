49e3fe040904990691fc50fec76ccc9a9a4f28bb
Colby Harper <colby.harper11@gmail.com>
2022-05-03 16:12:49 -0500

    add Pages folder and Home.js file

--- earnote-ui/src/App.js
+++ earnote-ui/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Home from './Pages/Home.js';
 
 function App() {
   return (
     <div>
+      <Home />
     </div>
   );
 }
---
+++ earnote-ui/src/Pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <div>Ear Note</div>
+    );
+};
+
+export default Home;
