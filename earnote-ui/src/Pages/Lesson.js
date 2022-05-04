dba40866acb34f01a51d959de8b77bc68544601f
Colby Harper <colby.harper11@gmail.com>
2022-05-03 19:17:41 -0500

    initial working audio button

--- earnote-ui/src/Pages/Lesson.js
+++ earnote-ui/src/Pages/Lesson.js
@@ -1,6 +1,17 @@
+import { Button } from '@mui/material';
+
 const Lesson = () => {
+
+    const handlePlay = () => {
+        var audio = new Audio('audio/nylon-guitar-shot-a-note.wav');
+        audio.play();
+    };
+
     return (
-        <div>Lesson Page</div>
+        <div>
+            <span>Lesson Page</span>
+            <Button onClick={handlePlay}>Test</Button>
+        </div>
     );
 };
 
