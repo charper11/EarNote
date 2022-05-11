d28f8fe088b52b9ac982f96e07b0962ec543d0b7
Colby Harper <colby.harper11@gmail.com>
2022-05-10 21:45:18 -0500

    remove dead code

--- earnote-ui/src/Pages/Home.js
+++ earnote-ui/src/Pages/Home.js
@@ -1,28 +1,6 @@
-import { Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 import Roadmap from '../Components/Roadmap.js';
 
 const Home = ({fetchQuestions, setScore, setLessonKey}) => {
-
-  const navigate = useNavigate();
-
-  /*
-  const handleClick = (i) => {
-    setUnit(i);
-    navigate("/unit");
-  }
-
-  <Button onClick={() => handleClick(1)}>Unit 1 (beginner)</Button>
-
-
-  /*
-            <Roadmap 
-            fetchQuestions={fetchQuestions}
-            setScore={setScore}
-            setUnit={setUnit}
-            setLessonKey={setLessonKey}
-          />
-  */
 
     return (
         <div>
