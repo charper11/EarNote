6cf95c90d0c315e66852f3da5d09d2d6fca6f24b
Colby Harper <colby.harper11@gmail.com>
2022-05-12 18:52:02 -0500

    add colorBlind switch and language option buttons to settings

--- earnote-ui/src/Components/SettingsPopUp.js
+++ earnote-ui/src/Components/SettingsPopUp.js
@@ -2,15 +2,35 @@ import './SettingsPopUp.css';
 
 const SettingsPopUp = ({toggleSettings}) => {
 
-    const handleClick = () => {
+    const handleClose = () => {
         toggleSettings();
+    }
+
+    const setLanguage = (lang) => {
+        lang ? localStorage.setItem('isSpanish', lang) : localStorage.removeItem('isSpanish');
+    }
+
+    const setColorMode = () => {
+        localStorage.getItem('isColorBlindSet') ? localStorage.removeItem('isColorBlindSet') : localStorage.setItem('isColorBlindSet', true);
     }
 
     return (
         <div className="modal">
             <div className="modal_content">
-                <span className="close" onClick={handleClick}>&times;</span>
-                <p>I'm a pop up</p>
+                <span className="close" onClick={handleClose}>&times;</span>
+                <h2>Settings</h2>
+                <div>
+                  <span>Color Blind Mode</span>
+                  <label className="switch">
+                    <input type="checkbox" onClick={setColorMode} />
+                    <span className="slider round"></span>
+                  </label>
+                </div>
+                <div>
+                    <span>Language</span>
+                    <button onClick={() => setLanguage(false)}>English</button>
+                    <button onClick={() => setLanguage(true)}>Español</button>
+                </div>
             </div>
         </div>
     );
