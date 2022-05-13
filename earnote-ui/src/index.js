486649fd2f20893708fc4272cbd6598e99927d47
Colby Harper <colby.harper11@gmail.com>
2022-05-12 22:55:24 -0500

    fonts

--- earnote-ui/src/index.js
+++ earnote-ui/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './Fonts/Concert_One/ConcertOne-Regular.ttf';
+import './Fonts/Rubik_Glitch/RubikGlitch-Regular.ttf';
+import './Fonts/Titan_One/TitanOne-Regular.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
