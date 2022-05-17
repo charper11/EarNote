e27c7dbb153ef6a3674f8042a07678abade80fba
Colby Harper <colby.harper11@gmail.com>
2022-05-17 18:09:24 -0500

    sections of guide

--- earnote-ui/src/Pages/Guide.js
+++ earnote-ui/src/Pages/Guide.js
@@ -6,6 +6,9 @@ const Guide = () => {
     return (
         <div className="guide">
           <h2>Guide</h2>
+          <h3>The Circle of Fifths</h3>
+          <h3>Scale</h3>
+          <h3>Triads</h3>
           <Link to="/">Return to Home Page</Link>
         </div>
     );
