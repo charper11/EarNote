3503e1267a5468110cc02bd3d58420ae49474e27
Colby Harper <colby.harper11@gmail.com>
2022-05-16 21:43:18 -0500

    add css files for about and guide

--- earnote-ui/src/Pages/About.js
+++ earnote-ui/src/Pages/About.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import './About.css';
 
 const About = () => {
 
     return (
-        <div>
+        <div className='about'>
+          <h2>About</h2>
+          <p>My name is Colby.</p>
           <Link to="/">Return to Home Page</Link>
         </div>
     );
--- earnote-ui/src/Pages/Guide.js
+++ earnote-ui/src/Pages/Guide.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import './Guide.css';
 
 const Guide = () => {
 
     return (
-        <div>
+        <div className="guide">
+          <h2>Guide</h2>
           <Link to="/">Return to Home Page</Link>
         </div>
     );
