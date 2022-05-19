a2dd5add850663c77701d6ad7acdc09a95193084
Colby Harper <colby.harper11@gmail.com>
2022-05-18 23:43:01 -0500

    add notes to wheel

--- earnote-ui/src/Components/CircleOfFifths.js
+++ earnote-ui/src/Components/CircleOfFifths.js
@@ -8,20 +8,31 @@ const CircleOfFifths = () => {
                 <defs>
                   <circle id="c" className="breaks" r="40"/>
                 </defs>
-                <use xlinkHref='#c' className='red'/>
-                <use xlinkHref="#c" className='gold'/>
-                <use xlinkHref="#c" className='blue'/> 
-                <use xlinkHref="#c" className='yellow'/>
-                <use xlinkHref="#c" className='green'/>
-                <use xlinkHref="#c" className='black'/>
-                <use xlinkHref="#c" className='gray'/>
-                <use xlinkHref="#c" className='purple'/>
-                <use xlinkHref="#c" className='brown'/>
-                <use xlinkHref="#c" className='magenta'/>
-                <use xlinkHref="#c" className='teal'/>
-                <use xlinkHref="#c" className='orange'/>
+                <use xlinkHref='#c' className='a-note'/>
+                <use xlinkHref="#c" className='d-note'/>
+                <use xlinkHref="#c" className='g-note'/>
+                <use xlinkHref="#c" className='c-note'/>
+                <use xlinkHref="#c" className='f-note'/>
+                <use xlinkHref="#c" className='bb-note'/>
+                <use xlinkHref="#c" className='eb-note'/>
+                <use xlinkHref="#c" className='ab-note'/>
+                <use xlinkHref="#c" className='db-note'/>
+                <use xlinkHref="#c" className='gb-note'/>
+                <use xlinkHref="#c" className='b-note'/>
+                <use xlinkHref="#c" className='e-note'/>
 
-                <text className='text' x="40">red</text>
+                <text className='text' y="-40">C</text>
+                <text className='text' x="20" y="-34.6">G</text>
+                <text className='text' x="34.6" y="-20">D</text>
+                <text className='text' x="40">A</text>
+                <text className='text' x="34.6" y="20">E</text>
+                <text className='text' x="20" y="34.6">B</text>
+                <text className='text' y="40">Gb</text>
+                <text className='text' x="-20" y="34.6">Db</text>
+                <text className='text' x="-34.6" y="20">Ab</text>
+                <text className='text' x="-40">Eb</text>
+                <text className='text' x="-34.6" y="-20">Bb</text>
+                <text className='text' x="-20" y="-34.6">F</text>
             </svg>
         </div>
     );
