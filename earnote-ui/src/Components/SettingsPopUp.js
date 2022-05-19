57237e758fbb4b80a5d4a6d6608ddd4b80dad6ef
Colby Harper <colby.harper11@gmail.com>
2022-05-19 16:00:17 -0500

    add pass criteria setting

--- earnote-ui/src/Components/SettingsPopUp.js
+++ earnote-ui/src/Components/SettingsPopUp.js
@@ -15,24 +15,39 @@ const SettingsPopUp = ({toggleSettings}) => {
         localStorage.getItem('isColorBlindSet') ? localStorage.removeItem('isColorBlindSet') : localStorage.setItem('isColorBlindSet', true);
     }
 
+    const removeCriteria = () => {
+        if(localStorage.getItem('passCriteria')) {
+            localStorage.removeItem('passCriteria');
+        }
+    }
+
+    const setCriteria = () => {
+        localStorage.setItem('passCriteria', 5);
+    }
+
     return (
         <div className="modal">
             <div className="modal_content">
                 <span className="close" onClick={handleClose}>&times;</span>
                 <div className='center'>
-                <h2>Settings</h2>
-                <div className='content-list'>
-                  <span className='content-title'>Color Blind Mode</span>
-                  <label className="switch">
-                    <input type="checkbox" onClick={setColorMode} defaultChecked={localStorage.getItem('isColorBlindSet')==='true'} />
-                    <span className="slider round"></span>
-                  </label>
-                </div>
-                <div className='content-list'>
-                    <span className='content-title'>Language</span>
-                    <button className={localStorage.getItem('isSpanish') ? 'settings-button' : 'settings-button selected'} onClick={() => setLanguage(false)}>English</button>
-                    <button className={localStorage.getItem('isSpanish') ? 'settings-button selected' : 'settings-button'} onClick={() => setLanguage(true)}>Español</button>
-                </div>
+                    <h2>Settings</h2>
+                    <div className='content-list'>
+                    <span className='content-title'>Color Blind Mode</span>
+                    <label className="switch">
+                        <input type="checkbox" onClick={setColorMode} defaultChecked={localStorage.getItem('isColorBlindSet')==='true'} />
+                        <span className="slider round"></span>
+                    </label>
+                    </div>
+                    <div className='content-list'>
+                        <span className='content-title'>Language</span>
+                        <button className={localStorage.getItem('isSpanish') ? 'settings-button' : 'settings-button selected'} onClick={() => setLanguage(false)}>English</button>
+                        <button className={localStorage.getItem('isSpanish') ? 'settings-button selected' : 'settings-button'} onClick={() => setLanguage(true)}>Español</button>
+                    </div>
+                    <div className='content-list'>
+                        <span className='content-title'>Pass Criteria</span>
+                        <button onClick={removeCriteria}>None</button>
+                        <button onClick={setCriteria}>50%</button>
+                    </div>
                 </div>
             </div>
         </div>
--- earnote-ui/src/Pages/Result.js
+++ earnote-ui/src/Pages/Result.js
@@ -4,7 +4,19 @@ import './Result.css';
 const Result = ({score, lessonKey, lessonUnit}) => {
 
     // localStorage objects are {A1 : true}, {A2 : true}, etc...
-    localStorage.setItem(lessonKey + lessonUnit.toString(), true);
+    // check if score criteria is set
+    if(localStorage.getItem('passCriteria')) {
+      console.log("criteria set");
+      if(score >= localStorage.getItem('passCriteria')) {
+        localStorage.setItem(lessonKey + lessonUnit.toString(), true);
+      }
+      else {
+        console.log("score too low");
+      }
+    }
+    else {
+      localStorage.setItem(lessonKey + lessonUnit.toString(), true);
+    }
 
     return (
         <div className='result'>
