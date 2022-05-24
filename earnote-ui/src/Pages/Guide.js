05df466a38dff557e176135e01a0606e3b855bb4
Colby Harper <colby.harper11@gmail.com>
2022-05-23 22:02:55 -0500

    for large screens have circle and text side by side in guide page

--- earnote-ui/src/Pages/Guide.js
+++ earnote-ui/src/Pages/Guide.js
@@ -8,10 +8,14 @@ const Guide = () => {
         <div className="guide">
           <h1>Guide</h1>
           <h2>The Circle of Fifths</h2>
-          <CircleOfFifths />
-          <p>Starting with C and moving clockwise note to note (C to G to D to A, etc), you'll notice that after 12 notes we return back to our starting point. This cyclical nature is called the circle of fifths and is one of the most fundamental principles in all of music theory.</p>
-          <p>If you pick one of the 12 notes in the circle, it along with the note to the left and the right make up what is called the three primary chords of that key.</p>
-          <p>Ear trainer progresses through this circle, with each lesson covering one set of primary chords at a time.</p>
+          <div className="float-container">
+          <div className="float-child"><CircleOfFifths /></div>
+          <div className='float-child'>
+          <p className='circle-p'>Starting with C and moving clockwise note to note (C to G to D to A, etc), you'll notice that after 12 notes we return back to our starting point. This cyclical nature is called the circle of fifths and is one of the most fundamental principles in all of music theory.</p>
+          <p className='circle-p'>If you pick one of the 12 notes in the circle, it along with the note to the left and the right make up what is called the three primary chords of that key.</p>
+          <p className='circle-p'>Ear trainer progresses through this circle, with each lesson covering one set of primary chords at a time.</p>
+          </div>
+          </div>
           <h2>Scale</h2>
           <p>A scale is a set of notes arranged in series, from one note to the same note an octave higher. Think, "Do, re, mi, fa, so, la, ti, do."</p>
           <p>Some questions in Ear Trainer will ask you to identify the same note but in a higher or lower octave.</p>
