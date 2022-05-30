48c89f52f32a6db76fe6204af2e6920eacf05bbd
Colby Harper <colby.harper11@gmail.com>
2022-05-30 15:12:52 -0500

    add pass/fail message

--- earnote-ui/src/Pages/Result.js
+++ earnote-ui/src/Pages/Result.js
@@ -3,24 +3,28 @@ import './Result.css';
 
 const Result = ({score, lessonKey, lessonUnit}) => {
 
+    let passOrFail = "";
+
     // localStorage objects are {A1 : true}, {A2 : true}, etc...
     // check if score criteria is set
     if(localStorage.getItem('passCriteria')) {
-      console.log("criteria set");
       if(score >= localStorage.getItem('passCriteria')) {
         localStorage.setItem(lessonKey + lessonUnit.toString(), true);
+        passOrFail = "you passed!";
       }
       else {
-        console.log("score too low");
+        passOrFail = "you did not score high enough, try again"
       }
     }
     else {
       localStorage.setItem(lessonKey + lessonUnit.toString(), true);
+      passOrFail = "you passed!";
     }
 
     return (
         <div className='result'>
-          <span className='score'> Score: {score} </span>
+          <span className='score'> Score: {score}/10 </span>
+          <p>{passOrFail}</p>
           <Link className='unitLink' to="/">Return to Home Page</Link>
         </div>
     );
