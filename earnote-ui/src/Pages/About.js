b709043a8424b843450d2d020cfd3eaf199d707e
Colby Harper <colby.harper11@gmail.com>
2022-07-22 20:37:09 -0500

    add language options to About page

--- earnote-ui/src/Pages/About.js
+++ earnote-ui/src/Pages/About.js
@@ -1,25 +1,25 @@
 import { Link } from 'react-router-dom';
 import './About.css';
 
-const About = () => {
+const About = ({langObj}) => {
 
     return (
         <div className='about'>
-          <h1>About</h1>
-          <h2>What is Ear Trainer?</h2>
-          <p>The Ear Trainer program is a fun and easy way to start learning how to recognize notes just by hearing them.</p>
-          <h2>How do I learn with Ear Trainer?</h2>
-          <p>The ear trainer program takes you on a path around the Circle of Fifths, learning notes in one chord at a time.</p>
-          <p>There are three units each with 12 lessons (one for each key). Each unit builds on the last and increases in difficulty as you increase in skill.</p>
-          <h2>How is progress tracked?</h2>
-          <p>Once a lesson is completed, your progress will be updated. How to determine what constitutes a completed lesson can be applied in the settings. By default, completing all 10 questions regardless of score will award a completed lesson.</p>
-          <h2>Why use Ear Trainer?</h2>
-          <p>A strong inner ear is essential for understanding music composition. Great musicians have advanced listening skills that improve the quality of their performances, as well as the music creation process.</p>
-          <h2>Who made Ear Trainer?</h2>
-          <p>Hey, I'm Colby. I'm a software developer who recently decided to learn music but was struggling to improve my 'inner ear'. I couldn't find anything that directly helped with this so I decided to make my own tool. Feel free to connect with me on <a href="https://www.linkedin.com/in/colbyharpersantaclara">LinkedIn</a>!</p>
-          <h2>Interested in the code?</h2>
-          <p>Check it out on github <a href="https://github.com/charper11/EarTrainer">here</a>.</p>
-          <Link className="return" to="/"><h3>Return to Home Page</h3></Link>
+          <h1>{langObj.about}</h1>
+          <h2>{langObj.About.what}</h2>
+          <p>{langObj.About.what_content}</p>
+          <h2>{langObj.About.how_do_i_learn}</h2>
+          <p>{langObj.About.how_do_i_learn_content1}</p>
+          <p>{langObj.About.how_do_i_learn_content2}</p>
+          <h2>{langObj.About.how_is_progress}</h2>
+          <p>{langObj.About.how_is_progress_content}</p>
+          <h2>{langObj.About.why}</h2>
+          <p>{langObj.About.why_content}</p>
+          <h2>{langObj.About.who}</h2>
+          <p>{langObj.About.who_content} <a href="https://www.linkedin.com/in/colbyharpersantaclara">LinkedIn</a>!</p>
+          <h2>{langObj.About.interested}</h2>
+          <p>{langObj.About.interested_content} <a href="https://github.com/charper11/EarTrainer">{langObj.About.here}</a>.</p>
+          <Link className="return" to="/"><h3>{langObj.About.return}</h3></Link>
         </div>
     );
 };
